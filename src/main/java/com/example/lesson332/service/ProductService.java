04254051516681e1a1c0ca44ddb2e148de14d1ab
asrytikov <asrytikov@gmail.com>
2023-12-05 20:55:17 +0400

    Lesson 33

--- src/main/java/com/example/lesson332/service/ProductService.java
+++ src/main/java/com/example/lesson332/service/ProductService.java
@@ -1,2 +1,23 @@
-package com.example.lesson332.service;public class ProductService {
+package com.example.lesson332.service;
+
+import com.example.lesson332.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public List<Product> findAll(){
+        return products;
+    }
+
+
 }
